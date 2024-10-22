4b4e04ed203882b70128d95ef8c30479877b6fbf
Max Candocia <mscandocia@gmail.com>
2024-10-22 17:19:35 -0500

    added parameters and part of transformed_parameters; some fixes in code, incl. with kernels and edges

--- segments.py
+++ segments.py
@@ -1,7 +1,11 @@
 import re
+
+from tools import clns
+
 from tools import degree_derivative_iter_max
 from tools import degree_derivative_iter_sum
 
+from subsegments import initialize_kernel
 from subsegments import build_kernel_denominator_loop
 from subsegments import build_kernel_loop
 
@@ -62,8 +66,8 @@ def build_data_segment(options):
     if options['use_perfect_grid']:
         # note: if edges are defined, then
         # expanded version of mask will be defined later
-        mask = f'mask = matrix[ydim][xdim]'
-        known_map = f'known_map = matrix[ydim][xdim]'
+        mask = f'mask = matrix[ydim,xdim]'
+        known_map = f'known_map = matrix[ydim,xdim]'
         xcoords = ''
         ycoords = ''
         phi_if_defined = ''
@@ -108,6 +112,9 @@ def build_data_segment(options):
     else:
         defined_derivatives = ''
 
+    degree = options['degree']
+    required_derivative_names = degree_derivative_iter_max(degree // 2)        
+
     if options['existing_edges']:
         top = 'T' in options['existing_edges']
         bot = 'B' in options['existing_edges']
@@ -120,14 +127,15 @@ def build_data_segment(options):
 
         # add edges for each combination of derivative
         # (e.g., edge_T_x0y0, edge_B_x1y2
-        if top:
-            edge_list.append(f'row_vector[xdim]edge_top;')
-        if bot:
-            edge_list.append(f'row_vector[xdim]edge_bot;')
-        if left:
-            edge_list.append(f'vector[ydim]edge_left;')
-        if right:
-            edge_list.append(f'vector[ydim]edge_right;')
+        for name in required_derivative_names:
+            if top:
+                edge_list.append(f'row_vector[xdim]edge_top_{name};')
+            if bot:
+                edge_list.append(f'row_vector[xdim]edge_bot_{name};')
+            if left:
+                edge_list.append(f'vector[ydim]edge_left_{name};')
+            if right:
+                edge_list.append(f'vector[ydim]edge_right_{name};')
 
         edge_definitions = '\n'.join(edge_list)
     else:
@@ -217,46 +225,231 @@ def build_transformed_data_segment(options):
 PARAMETER_TEMPLATE = """
 // parameter grid w/necessary truncations for each derivative
 // if kernels used, rename this with `_raw` 
+{parameter_grid_definitions}
 
+// kernel matrices
+{kernel_definitions}
 
 //  if z-drift is being used, define the errors and the sigmas
 //  (also allow sigmas to be defined)
+{zdrift_definitions}
 
 //  if xy-drift is being used, define matrix of drifts
 //  sigma should be defined in `data` block, not here
+{xydrift_definitions}
 
 // future: define additional variates used in model
 
 """
 
 def build_parameter_segment(options):
-    return '[PLACEHOLDER]'
+    degree = options['degree']
+    # determine # of top and bottom portions of grid that are sliced off
+    if options['existing_edges'] or options['kernel_opts']:
+        top = 'T' in options['existing_edges']
+        bot = 'B' in options['existing_edges']
+        left = 'L' in options['existing_edges']
+        right = 'R' in options['existing_edges']
+        n_vertical = top + bot
+        n_horizontal = left + right
+        sfx = '_raw'
+    else:
+        n_vertical = 0
+        n_horizontal = 0
+        sfx = ''
+
+    required_derivative_names = degree_derivative_iter_max(degree // 2)
+
+    for name in required_derivative_names:
+        grid_definition_list.append(
+            f'matrix[ydim-{n_vertical},xdim-{n_horizontal}] map{sfx}_{name};'
+        )
+
+    parameter_grid_definitions = '\n'.join(grid_definition_list)
+
+    if options['kernel_opts']:
+        lines = []
+        for i, opt in enumerate(options['kernel_opts']):
+            lines.append(initialize_kernel(opt,i))
+        kernel_definitions = '\n'.join(lines)
+            
+    else:
+        kernel_definitions = ''
+
+    # z-drift
+    if options['phi'] or options['estimate_phi']:
+        use_phi = True
+    else:
+        use_phi = False
+
+    if use_phi:
+        zdrift_definitions = clns("""
+        // z-drift
+        vector[N] e_zdrift;
+        real e_zdrift0;
+        // time delta
+        vector[N] td;
+        """)
+    else:
+        zdrift_definitions = ''
+
+    # xy-drift
+    if options['drift_sd']:
+        xydrift_defintions = clns("""
+        vector[N] xdrift;
+        vector[N] ydrift;
+        real xdrift0;
+        real ydrift0;
+        """)
+    else:
+        xydrift_definitions = ''
+
+    parameter_segment = PARAMETER_TEMPLATE.format(
+        parameter_grid_definitions=parameter_grid_definitions,
+        kernel_definitions = kernel_definitions,
+        zdrift_definitions = zdrift_definitions,
+        xydrift_definitions = xydrift_definitions,
+    )
+
+    return parameter_segment
+    
 
 TRANSFORMED_PARAMETER_TEMPLATE = """
 // append edges to parameters for each derivative (if applicable)
+// also defines non-raw matrix if kernels are used
+{edge_appends}
 
 // calculate kernel contributions (if applicable)
+{kernel_contributions}
 
 // calculate sub-blocks for each derivative {{00,01,10,11}}
+{subblock_definitions}
 
 // calculate a-values for spline
+{a_values}
 
 // calculate squared derivatives used for penalties
+{squared_derivatives}
 
 // if z-drift is being used, define cumulative error with time drift component
+{zdrift_error}
 
 // if xy-drift is being used, calcualte probabilities of transitions
+{xydrift_error}
 
 // calculate the estimates for each point
 // if xy-drift is allowed, first determine the cell 
 // future: include effects of variates in the model
+{estimates_calculation}
 
 // calculate overall penalty
+{penalty_calculation}
 
 """
 
 def build_transformed_parameter_segment(options):
-    return '[PLACEHOLDER]'
+    degree = options['degree']
+    required_derivative_names = degree_derivative_iter_max(degree // 2)
+    
+    if options['existing_edges'] or options['kernel_opts']:
+        top = 'T' in options['existing_edges']
+        bot = 'B' in options['existing_edges']
+        left = 'L' in options['existing_edges']
+        right = 'R' in options['existing_edges']
+        n_vertical = top + bot
+        n_horizontal = left + right
+
+        # loop with appends
+        lines = []
+        for name in required_derivative_names:
+            base_name = f'map_{name}'
+            raw_name = f'{base_name}_raw'
+            lines.append(
+                f'matrix[ydim,xdim] {base_name};'
+            )
+            if top:
+                vname = f'edge_top_{name}'
+                top_append = clns(f"""
+                for (i in 1:xdim){
+                {base_name}[1,i] = {vname}[i];
+                }
+                """)
+                tb = '2'
+                lines.append(top_append)
+            else:
+                tb = '1'
+            if bot:
+                vname = f'edge_bot_{name}'
+                bot_append = clns(f"""
+                for (i in 1:xdim){
+                {base_name}[ydim,i] = {vname}[i];
+                }
+                """)
+                bb = 'ydim-1'
+                lines.append(bot_append)
+            else:
+                bb = 'ydim'
+            if left:
+                vname = f'edge_left_{name}'
+                bot_append = clns(f"""
+                for (i in 1:ydim){
+                {base_name}[i,1] = {vname}[i];
+                }
+                """)
+                lines.append(left_append)
+                lb = '2'
+            else:
+                lb = '1'
+                
+            if right:
+                vname = f'edge_right_{name}'
+                bot_append = clns(f"""
+                for (i in 1:ydim){
+                {base_name}[i,xdim] = {vname}[i];
+                }
+                """)
+                lines.append(right_append)
+                rb = 'xdim-1'
+            else:
+                rb='xdim'
+            # center matrix
+            center_assignment = "{base_name}[{tb}:{bb},{lb}:{rb}] = {raw_name};"
+            lines.append(center_assignment)
+        edge_appends = '\n'.join(lines)
+
+    else:
+        n_vertical = 0
+        n_horizontal = 0
+        edge_appends = ''
+
+    # kernel contributions
+    if options['kernel_opts']:
+        lines = []
+        for degree_prefix in required_derivative_names:
+            for i, opt in enumerate(kernel_opts):
+                varname = f'k{i}_{degree_prefix}'
+                lines.append(build_kernel_loop(opt,degree_prefix,varname,i)
+        kernel_contributions = '\n'.join(lines)
+            
+    else:
+        kernel_contributions = ''
+
+        
+    if options['phi'] or options['estimate_phi']:
+        use_phi = True
+    else:
+        use_phi = False
+
+    if use_phi:
+        zdrift_error = '[PLACEHOLDER]'
+    else:
+        zdrift_definitions = ''
+
+    # xy-drift
+    if options['drift_sd']:
+        xydrift_error = '[PLACEHOLDER]'
+    else:
+        xydrift_error = ''
 
 MODEL_TEMPLATE = """
 // to help stabilize parameters
--- subsegments.py
+++ subsegments.py
@@ -1,6 +1,8 @@
 import math
 import re
 
+from tools import clns
+from tools import clear_empty
 from tools import extract_degree
 
 # this file contains additional structure-creating code
@@ -101,18 +103,19 @@ def build_kernel_denominator_loop(opt,degree_prefix,varname,ki):
     }}
     """
 
+    # TODO: omit kernel denominators if flag is disabled
     # assigns kernel sizes
     # skips re-defining kernels of different orders
     base_definitions = kd_base_template.format(
         title=title,
-        kernel_dimensions = '\n'.join([
+        kernel_dimensions = '\n'.join(clear_empty([
             ksyi * (d==0),
             ksxi * (d==0),
             ksyia * (d==0),
             ksxia * (d==0),
             kmxvi,
             kmxvia,            
-        ])
+        ]))
     )
 
     # square (T=0)
@@ -159,5 +162,142 @@ def build_kernel_denominator_loop(opt,degree_prefix,varname,ki):
 
     return full_kernel_initialization
 
+def initialize_kernel(opt, ki):
+    kD, kR, kT, dflag, max_range, normal_sd = opt    
+    vname = f'kernel_{ki}'
+    kxv = f'kx_{ki}'
+    kyv = f'ky_{ki}'
+    return clns("""
+    matrix[{kyv},{kxv}] {vname};
+    """)
+
+klT0_template = """
+{title}
+// template for squares
+for (yi in 1:{nky}){{
+  for (xi in 1:{nkx}){{
+    // for each subkernel loop
+        for (y in max({tb}, 1+(yi - 1) * {kD} - {max_range}):min({lb},1+(yi - 1) * {kD} + {max_range})){{
+          for (x in max({lb}, 1+(xi - 1) * {kD} - {max_range}):min({rb},1+(xi - 1) * {kD} + {max_range})){{
+            // note the center is (1+(yi-1)*{kD},(1+(xi-1)*{kD})
+            {mapname}[y,x] += {kname}[yi,xi] * {value_function};
+          }}
+        }}
+  }}
+}}
+
+"""
+
+klT1_template = """
+// TODO: develop later
+"""
+
+klT2_template = """
+// TODO: develop later
+"""
+
 def build_kernel_loop(opt,degree_prefix,varname,ki):
-    pass
+    kD, kR, kT, dflag, max_range, normal_sd = opt
+    degree_x = extract_degree(degree_prefix,'x')
+    degree_y = extract_degree(degree_prefix,'y')
+    d = degree_x + degree_y
+
+    top = 'T' in options['existing_edges']
+    bot = 'B' in options['existing_edges']
+    left = 'L' in options['existing_edges']
+    right = 'R' in options['existing_edges']
+    n_vertical = top + bot
+    n_horizontal = left + right
+    kname = f'kernel_{ki}'
+
+    if top:
+        tb = '2'
+    else:
+        tb = '1'
+    if bot:
+        bb = 'ydim'
+    else:
+        bb = 'ydim-1'
+    if left:
+        lb = '2'
+    else:
+        lb = '1'
+    if right:
+        rb = 'xdim-1'
+    else:
+        rb='xdim'
+
+    # this makes it easier to read the code
+    title=f"""//kernel #{ki+1} degrees {degree_prefix} (sum={d})|dflag={dflag}
+//tblr={tb}{bb}{lb}{rb}"""
+
+    # values to assign (number of kernels in each direction)
+    #ksx = f'xdim %/% {kD}+1;'
+    #ksy = f'ydim %/% {kD}+1;'
+
+    # IMPORTANT VARIABLES TO REUSE ELSEWHERE
+    kxv = f'kx_{ki}'
+    kyv = f'ky_{ki}'
+
+    # definition and assignemtn
+    #ksxi = f'int<lower=1> {kxv};'
+    #ksyi = f'int<lower=1> {kyv};'
+
+    #ksxia = f'{kxv} = {ksx};'
+    #ksyia = f'{kyv} = {ksy};'
+
+    # denominators
+    kmxv = f'kernel_d{degree_prefix}_{ki}'
+    #kmxvi = f'matrix<lower=0>[ydim,xdim] {kmxv};'
+
+    if kT == 0:
+        mapname = f'map_{degree_prefix}'        
+        if d == 0:
+            #value function
+            value_function = f'(1.0-{kR} * max(abs(y-(1+yi-1)*{kD}),abs(x-(1+(xi-1)*{kD}))))'
+            kernel_lines = klT0_template.format(
+                title=title,
+                nky=kyv,
+                nkx=kxv,
+                max_range = max_range,
+                mapname=mapname,
+                value_function=value_function,
+                kname=kname,
+            )
+        elif d == 1:
+            # x derivative
+            if degree_x == 1:
+                # if x < center, + * kernel strength
+                # if x > center, - * kernel strength
+                value_function = f'sign(1+(xi-1) * {kD} - x)'
+
+            # y derivative
+            if degree_y == 1:
+                # if y < center, + * kernel strength
+                # if y > center, - * kernel strength
+                value_function = f'sign(1+(yi-1) * {kD} - y)'
+            
+            kernel_lines = klT0_template.format(
+                title=title,
+                nky=kyv,
+                nkx=kxv,
+                max_range = max_range,
+                mapname=mapname,
+                value_function=value_function,
+                kname=kname,
+            )            
+        else:
+            kernel_lines = ''
+        
+    # diamond (T=1)
+    if kT == 1:
+        # evaluate max range (x + y)
+        raise NotImplementedError('Kernel of Type 1 (diamond) not yet implemented!')
+
+    # circle (T=2)
+    if kT == 2:
+        # evaluate max range (sqrt(x^2 + y^2))
+        raise NotImplementedError('Kernel of Type 2 (circle) not yet implemented!')
+
+    return kernel_lines
+
